83bdfe5c6f1bfee8412034463e2612c2900d8057
quyentx <quyentx@outlook.com>
2016-02-24 16:40:27 +0700

    quyentx-draft2: Deleted .xlsx file and some unused imports; added .xls file

--- src/test/java/com/vsii/tsc/TSCSelenium03/quyentx/MyNewTourTest.java
+++ src/test/java/com/vsii/tsc/TSCSelenium03/quyentx/MyNewTourTest.java
@@ -6,11 +6,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.AfterSuite;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -365,27 +361,8 @@ public class MyNewTourTest {
 
   @AfterClass
   public void afterClass() {
-	  System.out.println("After class");
+	  System.out.println("Class execution finished!");
 	  driver.close();
-  }
-
-  @BeforeTest
-  public void beforeTest() {
-  }
-
-  @AfterTest
-  public void afterTest() {
-	  
-  }
-
-  @BeforeSuite(alwaysRun = true)
-  public void beforeSuite() {
-
-  }
-
-  @AfterSuite(alwaysRun = true)
-  public void afterSuite() {
-	  
   }
   
   public void login() {
